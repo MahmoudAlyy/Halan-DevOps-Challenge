1e8d2cd8eca4f97d13342a2f43d7ebdce6e8ddb9
root <root@LAPTOP-9Q6E0JMG.localdomain>
2021-02-24 14:00:02 +0200

    checkpoint

---
+++ cloud/.terraform.lock.hcl
@@ -0,0 +1,37 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version = "3.57.0"
+  hashes = [
+    "h1:RKEcuVaag/sQfTHJPJt8okNXPe0a7kqcGoAtdvIB+1I=",
+    "zh:1c0ec65d86110643666fa88e39a06fd7f03ee959f2b528ab257e64b9333eca8e",
+    "zh:2b3f29bd8afb988a4f22d546dd44887b9a208126cb53992ebd9631a0b05b5032",
+    "zh:2cc38f03569813297cc182677446bf7c4d13cbdca0271cd82de319210796e045",
+    "zh:3fe2445757d4894866c956df692dbf6550f5e9607a66e60c5c61b2354c6e8661",
+    "zh:429e0af85347d7cbd06645a82bea2e60ea06fab65f31d1ee5f296e3c42652d8c",
+    "zh:79f58241ad877bc256eaab3935de93b8d5bc56506482c6545d06cf8ce11bb84d",
+    "zh:7c0dfe6edb05352b659e7bda2fa87dc29bbc3980d5c11b9e232859c818036bdd",
+    "zh:90310e7ac30938a0d4b0292fc3610c0d1b5c953240d26623fc4629626da436ce",
+    "zh:dbb8061f4b499bc6c72560c0ff8e79606281554432f0c3ff4a357a7d887f1253",
+    "zh:f84e0ddd520169961ec34c65406fd6a0e96ad1cce631fbd97f4b0ff83b6bd1e9",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.1.0"
+  hashes = [
+    "h1:EYZdckuGU3n6APs97nS2LxZm3dDtGqyM4qaIvsmac8o=",
+    "zh:0f1ec65101fa35050978d483d6e8916664b7556800348456ff3d09454ac1eae2",
+    "zh:36e42ac19f5d68467aacf07e6adcf83c7486f2e5b5f4339e9671f68525fc87ab",
+    "zh:6db9db2a1819e77b1642ec3b5e95042b202aee8151a0256d289f2e141bf3ceb3",
+    "zh:719dfd97bb9ddce99f7d741260b8ece2682b363735c764cac83303f02386075a",
+    "zh:7598bb86e0378fd97eaa04638c1a4c75f960f62f69d3662e6d80ffa5a89847fe",
+    "zh:ad0a188b52517fec9eca393f1e2c9daea362b33ae2eb38a857b6b09949a727c1",
+    "zh:c46846c8df66a13fee6eff7dc5d528a7f868ae0dcf92d79deaac73cc297ed20c",
+    "zh:dc1a20a2eec12095d04bf6da5321f535351a594a636912361db20eb2a707ccc4",
+    "zh:e57ab4771a9d999401f6badd8b018558357d3cbdf3d33cc0c4f83e818ca8e94b",
+    "zh:ebdcde208072b4b0f8d305ebf2bfdc62c926e0717599dcf8ec2fd8c5845031c3",
+    "zh:ef34c52b68933bedd0868a13ccfd59ff1c820f299760b3c02e008dc95e2ece91",
+  ]
+}
---
+++ cloud/main.tf
@@ -0,0 +1,165 @@
+variable "project_id" {
+  type = string
+  default = "my-project-11-305719"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+  credentials = file("source_account.json")
+}
+
+// enable apis  
+variable "project_services" {
+  default = [
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "services" {
+  count   = length(var.project_services)
+  project     = var.project_id
+  service    = var.project_services[count.index]
+  disable_dependent_services = true
+}
+
+// TODO concat nane with project id
+resource "google_compute_network" "vpc_network" {
+  name                    = "${var.project_id}-network"
+
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_instance" "api" {
+  name         = "api"
+  machine_type = "e2-small"
+
+  tags = ["web-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20210211"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "db-master" {
+  name         = "db-master"
+  machine_type = "e2-small"
+
+  tags = ["db-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20210211"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "db-slave" {
+  name         = "db-slave"
+  machine_type = "e2-small"
+
+  tags = ["db-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20210211"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "allow-ssh" {
+  name = "allow-ssh"
+  network = google_compute_network.vpc_network.self_link
+  //allow {
+  //  protocol = "icmp"
+  //}
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  // Allow traffic from everywhere 
+  source_ranges = ["0.0.0.0/0"]
+
+  //If no targetTags are specified, the firewall rule applies to all instances on the specified network.
+  //target_tags   = ["http-server"]
+}
+
+
+resource "google_compute_firewall" "web-server" {
+  name = "web-server"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80","443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags   = ["web-server"]
+}
+
+
+resource "google_compute_firewall" "db-server" {
+  name = "db-server"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags   = ["db-server"]
+}
+
+resource "google_compute_project_metadata" "ssh_keys" {
+    metadata = {
+    ssh-keys = "root:${file("test.pub")}"
+}
+}
+
+output "vm_name" {
+  value = google_compute_instance.api.name
+}
+output "public_ip" {
+  value = google_compute_instance.api.network_interface[0].access_config[0].nat_ip
+}
+output "private_ip" {
+  value = google_compute_instance.api.network_interface[0].network_ip
+}
+
+# generate inventory file for Ansible
+resource "local_file" "inventory" {
+  filename = "inventory"
+  content = <<-EOT
+    api ansible_host=${google_compute_instance.api.network_interface[0].access_config[0].nat_ip}
+    db-master ansible_host=${google_compute_instance.db-master.network_interface[0].access_config[0].nat_ip}
+    db-salve ansible_host=${google_compute_instance.db-slave.network_interface[0].access_config[0].nat_ip}
+  EOT
+}
